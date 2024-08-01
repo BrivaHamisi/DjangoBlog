0e70bd3c63bda9d03a90f502e0ad2618826a63ef
BrivaHamisi <brivahamisi@gmail.com>
2024-08-02 01:05:11 +0300

    Login and Logout

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,3 +127,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +9,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your Account has been created. You are Now able to Login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
 
